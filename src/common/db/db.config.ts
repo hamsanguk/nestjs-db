3202a89f83e3db5bf40fd8431bc171d57646a3a7
함상욱 <hamsang-ug@hamsang-ug-ui-MacBookPro-2.local>
2025-05-29 17:59:43 +0900

    nest-db

--- src/common/db/db.config.ts
+++ src/common/db/db.config.ts
@@ -5,13 +5,13 @@ export const dbConfig = (
   configService: ConfigService
 ): TypeOrmModuleOptions => {
   const config: TypeOrmModuleOptions = {
-    // Todo: DB Config를 완성시켜주세요.
+    // Todo: DB Config를 완성시켜주세요. ㅇOㅇ 
     type: 'postgres',
-    host: '',
-    port: 0,
-    username: '',
-    password: '',
-    database: '',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_DATABASE'),
     autoLoadEntities: true,
     synchronize: true,
   };
--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { User } from '../../../common/db/entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from '../dto/create-user.dto';
 
 @Injectable()
@@ -11,40 +11,48 @@ export class UserRepository {
     private readonly userRepository: Repository<User>
   ) {}
 
-  async findOne(options): Promise<User | undefined> {
+  async findOne(options): Promise<User | null> {
     // Todo: findOne은 어떤 조건이 들어올지 몰라도 where절에 options를 통해 데이터를 조회해야 합니다.
-    return undefined;
+    return this.userRepository.findOne({where:options});
   }
 
   async findAll(): Promise<User[]> {
     // Todo: findAll은 전체 데이터를 조회해야 합니다.
-    return [];
+    
+    return this.userRepository.find();
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     // Todo: create은 유저 데이터를 생성해야 합니다.
-    return {} as User;
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
   }
 
   async countAll(): Promise<number> {
     // Todo: countAll은 전체 데이터의 개수를 조회해야 합니다.
-    return 0;
+    return this.userRepository.count();
   }
 
   async existsByUserId(userId: string): Promise<boolean> {
     // Todo: existsByUserId은 userId로 유저의 정보가 있는지 확인해야 합니다. 리턴은 boolean으로 해주세요.
-    return false;
-  }
+    const count = await this.userRepository.count({where:{userId} });
+    return count>0;
+  }//count로 boolean return
 
   async update(
     userId: string,
     updateUserDto: Partial<CreateUserDto>
   ): Promise<User> {
     // Todo: update은 userId로 유저의 정보를 수정해야 합니다.
-    return {} as User;
+    const user = await this.userRepository.findOne({where:{userId}});
+    if(!user) throw new Error(`User with userId ${userId} not found`);
+    
+    Object.assign(user,updateUserDto);
+    return this.userRepository.save(user);
   }
 
   async delete(userId: string): Promise<void> {
     // Todo: delete은 userId로 유저의 정보를 삭제해야 합니다.
+    await this.userRepository.delete({userId});
   }
 }
